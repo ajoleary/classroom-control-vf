c700b8054d9475311d764dac32e20c339b26ce83
ajoleary <adamjoleary@gmail.com>
2016-02-23 15:32:23 -0800

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,30 @@
+class 'nginx'{
+package { 'nginx':
+ensure => present,
+}
+
+file { 'docroot':
+ensure => directory,
+path => " /var/www",
+}
+file { 'index':
+ensure => file,
+path => "/var/www/index.html",
+source => 'puppet:///modules/nginx/index.html',
+}
+
+file { 'config':
+ensure => file,
+path => '/etc/nginx/nginx.conf',
+source => 'puppet:///modules/nginx/index.html',
+}
+
+file { 'block';
+ensure => file,
+path => "/etc/nginx/conf.d/default.conf",
+source =>'puppet:///modules/nginx/nginx.conf,
+}
+
+
+
+}
