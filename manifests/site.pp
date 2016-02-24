ee95e5ac11fe32aaf4c1cc63291c8e5d5ef56952
ajoleary <adamjoleary@gmail.com>
2016-02-24 10:52:13 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,8 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   if $::virtual != 'physical' {
-$vmname = capitalize($::virtual)
-notify { "This is a ${vmname} virtual machine.": }
+$hyperv = capitalize($::virtual)
+notify { "My HyperV host is: ${hyperv}\n": }
 }
 
 #include skeleton
