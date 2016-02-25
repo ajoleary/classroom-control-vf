2d780c25d825d64af224704cf57bf779a42e198d
ajoleary <adamjoleary@gmail.com>
2016-02-25 11:29:45 -0800

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,4 @@
-class nginx (
-$root = '/srv/www/',
-)
-{
-case $::osfamily {
-'RedHat':{
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-$defaultdocroot = '/var/www'
-$confdir = '/etc/nginx'
-$blockdir = '/etc/nginx/conf.d'
-$logdir = '/var/log/nginx'
-$service = 'nginx'
-$user = 'nginx'
-}
-'Debian':{
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-$defaultdocroot = '/var/www'
-$confdir = '/etc/nginx'
-$blockdir = '/etc/nginx/conf.d'
-$logdir = '/var/log/nginx'
-$service = 'nginx'
-$user = 'www-data'
-}
-'windows':{
-    $package  = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-$defaultdocroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$blockdir = 'C:/ProgramData/nginx/conf.d'
-$logdir = 'C:/ProgramData/nginx/logs'
-$service = 'nginx'
-$user = 'nobody'
-}
-'default':{
-fail { "Get a supported operating system.":}
-}
-}
+
 
 $docroot = $root ? {
 undef => $default_docroot,
